a95af18dd4d909e1c242a244cc4e3b40f8a22c74
Naoto3615 <63333757+Naoto3615@users.noreply.github.com>
2021-04-01 22:33:29 +0900

    Initial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.16.0"
 
 set :application, "portfolio"
 set :repo_url, "git@github.com:Naoto3615/portfolio.git"
+set :branch, "main"
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, 'master'
