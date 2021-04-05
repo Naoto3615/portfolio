4c8a5df9238337e1062ae6d57619badc7a63244f
Naoto3615 <63333757+Naoto3615@users.noreply.github.com>
2021-04-05 16:09:17 +0900

    config complete

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '160.251.55.78', user: 'localadmin', roles: %w{app db web}, port: 55555 
-set :ssh_options, keys: '~/.ssh/id_rsa'
+server '118.27.9.129', user: 'localadmin', roles: %w{app db web}, port: 55555 
+set :ssh_options, keys: '~/.ssh/conoha/id_rsa'
