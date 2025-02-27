81c8c559a8f921936c0fc3e992de3c8eaeaaa82c
Naoto3615 <supra3615@gmail.com>
2025-02-27 15:53:38 +0900

    changes_all

--- Portfolio/config/settings.py
+++ Portfolio/config/settings.py
@@ -87,12 +87,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=False)
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
 
 
 # Password validation
