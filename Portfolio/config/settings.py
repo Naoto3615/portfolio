520c8ea30c7dec7b2bdfbdade445a76dc1ac0c8b
Naoto3615 <supra3615@gmail.com>
2025-02-27 17:01:07 +0900

    changes_all

--- Portfolio/config/settings.py
+++ Portfolio/config/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
     '.vercel.app',  # 他のサブドメインを許可
     'localhost',  # ローカル開発用
 ]
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS")
 # CSRF_TRUSTED_ORIGINS = ["https://portfolio-31z4ytouu-naoto3615s-projects.vercel.app"]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
