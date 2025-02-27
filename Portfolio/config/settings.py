8afafad0e175c25c4beed0f1427355cae1657f24
Naoto3615 <supra3615@gmail.com>
2025-02-27 14:28:42 +0900

    changeSSLconfig

--- Portfolio/config/settings.py
+++ Portfolio/config/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=False)
 }
 
 
